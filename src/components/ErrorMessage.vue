85ec8b7bacfc9f972f905925bb456423983ded9d
David Aji <davidenochaji@gmail.com>
2023-05-08 18:16:48 +0100

    add transition component

--- src/components/ErrorMessage.vue
+++ src/components/ErrorMessage.vue
@@ -1,13 +1,16 @@
 <template>
-  <div v-if="validator.$error">
-    <p class="error" v-for="error of validator.$errors" :key="error.$uid">
-      *{{ errorLabel }} {{ error.$message }}*
-    </p>
-  </div>
+  <FadeTransition>
+    <div v-if="validator.$error">
+      <p class="error" v-for="error of validator.$errors" :key="error.$uid">
+        *{{ errorLabel }} {{ error.$message }}*
+      </p>
+    </div>
+  </FadeTransition>
 </template>
 
 <script setup lang="ts">
 import type { ErrorObject } from '@vuelidate/core'
+import FadeTransition from './FadeTransition.vue'
 
 type PropsType = {
   validator: { $error: boolean; $errors: ErrorObject[] }
---
+++ src/components/FadeTransition.vue
@@ -0,0 +1,18 @@
+<template>
+  <Transition name="fade">
+    <slot></slot>
+  </Transition>
+</template>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateY(-50%);
+}
+</style>
